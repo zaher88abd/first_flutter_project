cdd77998950b3a13e4d74d6a6d07bb8a5205f237
Mohamad Zaher Abd Ulmoula <mh979648@dal.ca>
2021-03-08 13:08:58 -0400

    Use stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = ["What is your color?", "What is your cuntry"];
+
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello World!"));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text("Answer1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer2"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer3"),
+              onPressed: () => print("Chose answer  3"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 }
